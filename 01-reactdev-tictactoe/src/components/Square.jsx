aeb2ff39419b2440eb1d8ec775ca6cc726918bf0
francesca sandford <fran6san@gmail.com>
2023-07-21 22:52:20 +0100

    able to add X to all squares, updating the squares array

--- 01-reactdev-tictactoe/src/components/Square.jsx
+++ 01-reactdev-tictactoe/src/components/Square.jsx
@@ -1,11 +1,16 @@
 import PropTypes from 'prop-types';
 
-export const Square = ({ value }) => {
-  return <button className="square">{value}</button>
+export const Square = (props) => {
+  return (
+    <button className="square" onClick={props.onSquareClick}>
+      {props.value}
+    </button>
+  )
 }
 
 Square.propTypes ={
-  value: PropTypes.string
+  value: PropTypes.string,
+  onSquareClick: PropTypes.func
 }
 
 // export const Square = ({ value }) => {
