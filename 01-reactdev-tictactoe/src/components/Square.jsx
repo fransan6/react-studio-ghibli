5a8e37315d929fc52098262f1e7fb42f8c073b29
francesca sandford <fran6san@gmail.com>
2023-07-21 22:31:26 +0100

    start set up board knowing state of its squares

--- 01-reactdev-tictactoe/src/components/Square.jsx
+++ 01-reactdev-tictactoe/src/components/Square.jsx
@@ -1,18 +1,12 @@
-import { useState } from 'react';
+import PropTypes from 'prop-types';
 
-export const Square = () => {
-  const [value, setValue] = useState(null);
-  return (
-    <button className="square" onClick={ () => { setValue("X") } }>
-      {value}
-    </button>
-  )
+export const Square = ({ value }) => {
+  return <button className="square">{value}</button>
 }
 
-// import PropTypes from 'prop-types';
-// Square.propTypes ={
-//   value: PropTypes.string
-// }
+Square.propTypes ={
+  value: PropTypes.string
+}
 
 // export const Square = ({ value }) => {
 //   return <button className="square">{value}</button>;
