4241425f0cfa78728f6332bf8894a4bac1d5612a
francesca sandford <fran6san@gmail.com>
2023-07-22 19:57:53 +0100

    set up fetch from json

--- 00-reactdev-foodlist/src/App.jsx
+++ 00-reactdev-foodlist/src/App.jsx
@@ -1,6 +1,28 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("../public/db.json");
+      if (!response.ok) {
+        throw Error(`HTTP error: ${response.status} (${response.statusText})`)
+      }
+      const data = await response.json();
+      setData(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  console.log(data.length === 6 ? data[0].category : "Data is not available yet");
+
   return (
     <div></div>
   )
