568429185a367959a4110719cd348993a5b66c57
francesca sandford <fran6san@gmail.com>
2023-07-22 14:59:15 +0100

    finish game implementation

--- 01-reactdev-tictactoe/src/Game.jsx
+++ 01-reactdev-tictactoe/src/Game.jsx
@@ -1,19 +1,22 @@
 import { useState } from 'react';
 import { Board } from './components/Board';
-import './Game.css'
+import './Game.css';
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
   const handlePlay = (nextSquares) => {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
 
   const jumpTo = (nextMove) => {
-    nextMove
+    setCurrentMove(nextMove);
   }
 
   const moves = history.map((_, move) => {
