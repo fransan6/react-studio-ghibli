94c40592f94ccd37a3da71ba6bf6ca25e4b588cd
francesca sandford <fran6san@gmail.com>
2023-07-22 15:28:45 +0100

    refactor repetition in creating board

--- 01-reactdev-tictactoe/src/components/Board.jsx
+++ 01-reactdev-tictactoe/src/components/Board.jsx
@@ -24,26 +24,23 @@ export const Board = ({ xIsNext, squares, onPlay }) => {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
+  const createBoard = () => {
+    const boardSquares =[];
+    for (let i = 0; i < squares.length; i++) {
+      if (i % 3 === 0 ) {
+        boardSquares.push(<div className="board-row"></div>);
+      }
+      boardSquares.push(<Square value={squares[i]} onSquareClick={() => handleClick(i)} />);
+    }
+    return boardSquares;
+  }
+
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {createBoard()}
     </>
-  );
+  )
 }
 
 Board.propTypes ={
@@ -51,3 +48,24 @@ Board.propTypes ={
   squares: PropTypes.array,
   onPlay: PropTypes.func
 }
+
+// return (
+//   <>
+//     <div className="status">{status}</div>
+//     <div className="board-row">
+//       <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+//       <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+//       <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+//     </div>
+//     <div className="board-row">
+//       <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+//       <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+//       <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+//     </div>
+//     <div className="board-row">
+//       <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+//       <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+//       <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+//     </div>
+//   </>
+// );
