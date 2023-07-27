f1f74721ee9912c60c105a4b6a1058a3c25e6393
francesca sandford <fran6san@gmail.com>
2023-07-27 14:12:00 +0100

    set up fetching api data

--- 02-studioghibli-app/src/App.jsx
+++ 02-studioghibli-app/src/App.jsx
@@ -1,9 +1,31 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://ghibliapi.vercel.app/films');
+      if (!response.ok) {
+        throw Error(`HTTP error: ${response.status} (${response.statusText})`)
+      }
+      const apiData = await response.json();
+      setData(apiData);
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  console.log(data)
+
   return (
     <>
-      <div>Ciao</div>
+      <div>Ciao!</div>
     </>
   )
 }
