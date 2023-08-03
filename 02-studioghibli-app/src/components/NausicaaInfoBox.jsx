cf6bf62da96efd22e68677aa3a2f20e65d722f68
francesca sandford <fran6san@gmail.com>
2023-08-03 17:42:06 +0100

    feat(nausicaa): add component and style

---
+++ 02-studioghibli-app/src/components/NausicaaInfoBox.jsx
@@ -0,0 +1,9 @@
+import '../styles/NausicaaInfoBox.css'
+
+export default function NausicaaInfoBox() {
+  return (
+    <p className="nausicaa-info-box">
+      <b>Did you know...</b> It&apos;s commonly thought that <span className="bolder-text">Nausicaä of the Valley of the Wind</span>, directed by Hayao Miyazaki and based on his manga, was produced by Studio Ghibli. It was released in 1984, but Studio Ghibli formed in 1985 and its first film, <span className="bolder-text">Laputa: Castle in the Sky</span>, was released in 1986. However, many of those who worked on Nausicaä went on to form Studio Ghibli and it was Miyazaki&apos;s first of many collaborations with composer Joe Hisaishi. Despite not being producted by Studio Ghibli, it is typically considered part of the canon.
+    </p>
+  )
+}
